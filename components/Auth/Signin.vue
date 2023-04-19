670d06a5cc0b89c713dafd5e8cffa133af2ee61b
Ven <CareTiger@users.noreply.github.com>
2023-04-19 12:07:23 -0500

    updates

---
+++ components/Auth/Signin.vue
@@ -0,0 +1,113 @@
+<template>
+	<div class="flex flex-col space-y-6">
+		<div class="max-w-md mx-auto border border-gray-200 p-4 w-full">
+			<form @submit.prevent="handleStandardLogin">
+				<div class="flex flex-col space-y-6">
+					<div class="w-full">
+						<label for="email">Email:</label>
+						<input
+							type="email"
+							id="email"
+							placeholder="Your email"
+							v-model="email"
+						/>
+					</div>
+					<div>
+						<label for="password">Password:</label>
+						<input
+							type="password"
+							id="password"
+							placeholder="Password"
+							v-model="password"
+						/>
+					</div>
+					<p>
+						By signing in, I agree to the
+						<NuxtLink to="/privacy">Privacy Statement</NuxtLink> and
+						<NuxtLink to="/terms">Terms of Service</NuxtLink>.
+					</p>
+					<button type="submit" :disabled="loading">Sign In</button>
+				</div>
+			</form>
+		</div>
+
+		<div class="max-w-md mx-auto border border-gray-200 p-4 w-full">
+			<form @submit.prevent="handleOtpLogin">
+				<div class="flex flex-col space-y-6">
+					<div>
+						<label for="emailOTP">Email:</label>
+						<input
+							type="email"
+							id="emailOTP"
+							placeholder="Your email"
+							v-model="emailOTP"
+						/>
+					</div>
+					<p>
+						By signing in, I agree to the
+						<NuxtLink to="/privacy">Privacy Statement</NuxtLink> and
+						<NuxtLink to="/terms">Terms of Service</NuxtLink>.
+					</p>
+
+					<button type="submit" :disabled="loading">
+						Sign In using Magic Link
+					</button>
+				</div>
+			</form>
+		</div>
+
+		<div class="max-w-md mx-auto border border-gray-200 p-4 w-full">
+			<p>or sign in with</p>
+
+			<button
+				@click="supabase.auth.signInWithOAuth({ provider: 'github' })"
+			>
+				Github
+			</button>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+import { useAccountStore } from "~/stores/account";
+const user = useSupabaseUser();
+const supabase = useSupabaseAuthClient();
+const accountStore = useAccountStore();
+const loading = ref(false);
+const email = ref("");
+const emailOTP = ref("");
+const password = ref("");
+const handleOtpLogin = async () => {
+	try {
+		loading.value = true;
+		const { error } = await supabase.auth.signInWithOtp({
+			email: emailOTP.value,
+		});
+		if (error) throw error;
+		alert("Check your email for the login link!");
+	} catch (error) {
+		alert(error);
+	} finally {
+		loading.value = false;
+	}
+};
+const handleStandardLogin = async () => {
+	try {
+		loading.value = true;
+		const { error } = await supabase.auth.signInWithPassword({
+			email: email.value,
+			password: password.value,
+		});
+		if (error) throw error;
+	} catch (error) {
+		alert(error);
+	} finally {
+		loading.value = false;
+	}
+};
+watchEffect(async () => {
+	if (user.value) {
+		await accountStore.init();
+		navigateTo("/dashboard", { replace: true });
+	}
+});
+</script>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -3,3 +3,9 @@
 		<h1>Dashboard</h1>
 	</div>
 </template>
+<script setup lang="ts">
+definePageMeta({
+	title: "Dashboard",
+	middleware: ["auth"],
+});
+</script>
