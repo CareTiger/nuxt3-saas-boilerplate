69a5e3d8e3d1678a4168b581179d659cafc3ad51
Ven <CareTiger@users.noreply.github.com>
2023-04-19 16:12:15 -0500

    updates to plocies

--- prisma/sql/reset.sql
+++ prisma/sql/reset.sql
@@ -67,3 +67,24 @@ after delete on auth.users for each row execute procedure public.handle_deleted_
 -- 3. RESET RLS POLICIES ON RELEVANT TABLES
 -- ENABLE RLS ON ALL TABLES AND CREATE RLS POLICIES FOR EACH TABLE
 --
+-- table public.account
+alter table public.account
+enable row level security;
+
+-- table public.note
+alter table public.note
+enable row level security;
+
+-- table public.profile
+alter table public.profile
+enable row level security;
+
+CREATE POLICY "Enable CRUD for users based on user_uid"
+ON public.profile
+FOR ALL USING (
+  auth.uid()::text = user_uid
+);
+
+-- table public.plan
+alter table public.plan
+enable row level security;
