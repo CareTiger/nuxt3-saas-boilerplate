9d3f60212735e30fdc56b93a6de0641040c4dff8
Ven <CareTiger@users.noreply.github.com>
2023-04-21 20:31:31 -0500

    cleaning up

--- supabase/functions/write-storage/index.ts
+++ supabase/functions/write-storage/index.ts
@@ -21,7 +21,7 @@ serve(async (req) => {
 		// Create a Supabase client with the Auth context of the logged in user.
 		const supabaseClient = createClient(
 			Deno.env.get("SUPABASE_URL"),
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0",
+			Deno.env.get("SUPABASE_KEY"),
 			// Create client with Auth context of the user that called the function.
 			// This way your row-level-security (RLS) policies are applied.
 			{
