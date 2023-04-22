edd322d204c5f2f52865603f47c13087c46009b8
Ven <CareTiger@users.noreply.github.com>
2023-04-21 20:24:06 -0500

    updates edge functions

--- supabase/functions/read-storage/index.ts
+++ supabase/functions/read-storage/index.ts
@@ -2,7 +2,7 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
-import { serve } from "https://deno.land/std@0.182.0/http/server.ts";
+import * as mod from "https://deno.land/std@0.184.0/http/server.ts";
 import { createClient } from "https://esm.sh/@supabase/supabase-js";
 // import { supabaseClient } from "https://deno.land/x/supabase_deno/mod.ts";
 
@@ -11,7 +11,7 @@ const corsHeaders = {
 	"Access-Control-Allow-Headers": "authorization, x-client-info, apikey",
 };
 
-serve(async (req) => {
+mod.serve(async (req) => {
 	// read a text file from storage and print its contents
 	// This is needed if you're planning to invoke your function from a browser.
 	if (req.method === "OPTIONS") {
@@ -38,7 +38,7 @@ serve(async (req) => {
 
 		const { data, error } = await supabaseClient.storage
 			.from("saas")
-			.download("users/017b69f2-e951-4b4c-9b22-1fa3505014cc/discord.png");
+			.download("Error.jpg");
 		if (error) throw error;
 
 		// file contents are returned as a blob, we can convert it to utf-8 text by calling text() method.
@@ -54,7 +54,7 @@ serve(async (req) => {
 	} catch (error) {
 		console.error(error);
 
-		return new Response(JSON.stringify({ error: error.message }), {
+		return new Response(JSON.stringify({ error: error }), {
 			headers: { ...corsHeaders, "Content-Type": "application/json" },
 			status: 400,
 		});
