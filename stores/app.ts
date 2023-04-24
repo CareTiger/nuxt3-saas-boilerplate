066fc5324e30d4c5ef262d90d0da82687db97112
Ven <CareTiger@users.noreply.github.com>
2023-04-24 05:00:37 -0500

    updated the async function in the app store

--- stores/app.ts
+++ stores/app.ts
@@ -1,19 +1,25 @@
 import { defineStore, skipHydrate } from "pinia";
 import { Plan } from "@/types/global";
+import convertKeysToCamelCase from "~/utils";
 
 export const useAppStore = defineStore("app", () => {
 	// state
-	const plans = ref([]);
+	const plans = ref(<Plan[]>[]);
 
 	// actions
-	async function init() {
+	async function init(): Promise<void> {
 		try {
-			plans.value = await $fetch("/api/v1/app/getPlans", {
+			const response = await $fetch("/api/v1/app/getPlans", {
 				method: "GET",
 				headers: {
 					"Content-Type": "application/json",
 				},
 			});
+			const data = await response;
+			if (!data) {
+				throw new Error("Error getting plans");
+			}
+			plans.value = convertKeysToCamelCase(data);
 		} catch (error) {
 			console.log(error);
 		}
--- supabase/functions/read-storage/index.ts
+++ supabase/functions/read-storage/index.ts
@@ -22,9 +22,9 @@ serve(async (req) => {
 		// Create a Supabase client with the Auth context of the logged in user.
 		const supabaseClient = createClient(
 			// Supabase API URL - env var exported by default.
-			Deno.env.get("SUPABASE_URL") ?? "",
+			Deno.env.get("NUXT_SUPABASE_URL") ?? "",
 			// Supabase API ANON KEY - env var exported by default.
-			Deno.env.get("SUPABASE_KEY") ?? "",
+			Deno.env.get("NUXT_SUPABASE_KEY") ?? "",
 			// Create client with Auth context of the user that called the function.
 			// This way your row-level-security (RLS) policies are applied.
 			{
@@ -38,7 +38,7 @@ serve(async (req) => {
 
 		const { data, error } = await supabaseClient.storage
 			.from("saas")
-			.download("users/017b69f2-e951-4b4c-9b22-1fa3505014cc/discord.png");
+			.download("with_token.jpg");
 		if (error) throw error;
 
 		// file contents are returned as a blob, we can convert it to utf-8 text by calling text() method.
