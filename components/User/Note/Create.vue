a102bc975ed25922c293d50bf24c3d0cf4541c95
Ven <CareTiger@users.noreply.github.com>
2023-04-25 11:13:24 -0500

    fixed navigation blocking caused by tailwind class

--- components/User/Note/Create.vue
+++ components/User/Note/Create.vue
@@ -1,33 +1,5 @@
 <template>
-	<div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-		<div
-			class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-			aria-hidden="true"
-		>
-			<div
-				class="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-				style="
-					clip-path: polygon(
-						74.1% 44.1%,
-						100% 61.6%,
-						97.5% 26.9%,
-						85.5% 0.1%,
-						80.7% 2%,
-						72.5% 32.5%,
-						60.2% 62.4%,
-						52.4% 68.1%,
-						47.5% 58.3%,
-						45.2% 34.5%,
-						27.5% 76.7%,
-						0.1% 64.9%,
-						17.9% 100%,
-						27.6% 76.8%,
-						76.1% 97.7%,
-						74.1% 44.1%
-					);
-				"
-			></div>
-		</div>
+	<div class="isolate bg-white px-6 py-16 lg:px-8">
 		<div class="mx-auto max-w-2xl text-center">
 			<h2
 				class="text-3xl font-bold tracking-tight text-slate-900 sm:text-4xl"
@@ -38,7 +10,7 @@
 				Aute magna irure deserunt veniam aliqua magna enim voluptate.
 			</p>
 		</div>
-		<form action="#" method="POST" class="mx-auto mt-16 max-w-xl sm:mt-20">
+		<form action="#" method="POST" class="mx-auto mt-8 max-w-xl">
 			<div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
 				<div class="sm:col-span-2">
 					<label
